9e861b05c1572f0497faec6b686ddf498e61d293
seihmd <moku.said@gmail.com>
2018-08-16 23:17:00 +0900

    setup storybook

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "node": true
+  },
+  "extends": [
+    "plugin:vue/recommended",
+    "airbnb-base",
+    "prettier"
+  ],
+  "plugins": ["prettier"],
+  "parserOptions": {
+    "parser": "babel-eslint",
+    "sourceType": "module",
+    "allowImportExportEverywhere": true,
+    "codeFrame": false
+  },
+  "rules": {
+    "no-unused-vars": ["error", {
+      "args": "none"
+    }],
+    "prettier/prettier": ["error", {
+      "singleQuote": true
+    }]
+  }
+}
